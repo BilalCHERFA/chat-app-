f56a5f1a4254601d0184b05c8f175e7cea66b115
BilalCHERFA <cherfabilal@gmail.com>
2020-10-21 13:32:33 +0200

    React05 Le state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,47 @@
+// import { ReactComponent } from '*.svg';
+// import { render } from '@testing-library/react';
 import React from 'react'; 
 import './contact.css';
 
 
-function Contact(props) {
+class Contact extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  status: props.status,
+		};
+	}	
+
+	render() {
 	return (
 		<div className="Contact" >
-			<img className="avatar" src={props.avatar} alt={props.name} />
+			<img className="avatar" src={this.props.avatar} alt={this.props.name} />
 
 			<div>
-				<p className="name">{props.name}</p>
+				<p className="name">{this.props.name}</p>
+
 				<div className="status" >
-				<div className={(props.status)? "status-online" : "status-offline"} />
-				<p className="status-text">{(props.status)? 'Online' : 'Offline'} </p>
+					<span className={(this.state.status)? "status-online" : "status-offline"} 
+
+					onClick={event => {
+					const newStatus = !this.state.status;
+					this.setState({ status: newStatus });
+					}}> </span>
+
+					<p className= "status-text"
+
+					onClick={event => {
+					const newStatus = !this.state.status;
+					this.setState({ status: newStatus });
+					}} > {(this.state.status)? "Online" : "Offline"} </p>
+
+			   </div>
 			</div>
-			</div>
+
 		</div>
 		)
 	}
-
+}
 export default Contact; 
+
+
